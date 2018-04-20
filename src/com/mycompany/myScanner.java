9ece035d448cf3c17a2bd0edbf6740cb2e8b57ba
mohamedmaged <mohamedmaged35@yahoo.com>
2018-04-20 23:46:31 +0200

    enabled commet to be in multiple lines

--- src/com/mycompany/myScanner.java
+++ src/com/mycompany/myScanner.java
@@ -16,34 +16,38 @@ public class myScanner {
 
     //states:  1 = start, 2 = id, 2* = reserved, 3 = num, 4 = comment, 5 = symbol
     public void identifyTokens(String outputPath){
+        boolean flag=true;
         char c;
         String record;
         int num = dataInput.size();
         int i = 0;
+        String token=""; String is="";
         while(i < num){
             int j = 0;
             String line = dataInput.get(i);
             int counter = dataInput.get(i).length();
-            String token = "";
-            String is = "";
 
+            if(flag) {
+                 token = "";
+                 is = "";
+            }
             while(j < counter) {
                 c = line.charAt(j);
                 String st = Character.toString(c);
                 
-                if (" ".equals(st)){                                                     
+                if (" ".equals(st) && flag){
                      record = token + "\t"+"\t"+"\t" + is;
                     dataOutput.append(record + System.getProperty("line.separator"));
                     token = "";
                     is="";
                 }
-                else if ("".equals(st)){
+                else if ("".equals(st) && flag){
                      record = token + "\t"+"\t"+"\t" + is;
                     dataOutput.append(record + System.getProperty("line.separator"));
                     token = "";
                    // is="";
                 }
-                else if (";".equals(st)){
+                else if (";".equals(st) && flag){
                     record = token + "\t"+"\t"+"\t" + is;
                     String endLine = ";" + "\t"+"\t"+"\t" + "special character";
                     j++;
@@ -52,7 +56,7 @@ public class myScanner {
                     token = "";
                     is="";
                 }
-                else if (Character.isAlphabetic(c)){
+                else if (Character.isAlphabetic(c) && flag){
                     token += st;
                     if("if".equals(token) || "then".equals(token) || "else".equals(token) || "end".equals(token) || "repeat".equals(token) || "until".equals(token) || "read".equals(token) || "write".equals(token))
                             is = "reserved word";
@@ -62,27 +66,33 @@ public class myScanner {
                     }
 
                 }
-                else if (Character.isDigit(c)){
+                else if (Character.isDigit(c) &&flag){
                     if(is.equals(""))
                         is = "number";
                     token += st;
                 }
-                else if ("{".equals(st)){						
+                else if ("{".equals(st) | ! flag){
                     is = "comment";
                     while(c != '}'){
+                        if(j < counter )
                         c = line.charAt(j++);
+                        else {
+                            flag = false;
+                            break;
+                        }
                         st = Character.toString(c);
                         token += st;
+                        flag=true;
                     }
                     j--;
                 }
-                else if ("/".equals(st) || "*".equals(st) || "+".equals(st) || "-".equals(st)|| "=".equals(st)|| "<".equals(st)|| "(".equals(st)|| ")".equals(st)|| ":".equals(st)){
+                else if (("/".equals(st) || "*".equals(st) || "+".equals(st) || "-".equals(st)|| "=".equals(st)|| "<".equals(st)|| "(".equals(st)|| ")".equals(st)|| ":".equals(st)) && flag ){
                     is = "special character";
                     token += st;
                 }
 
                 j++;
-                if(j == counter && !(";".equals(token))){
+                if((j == counter && !(";".equals(token)))&& flag ){
                     if(!is.equals("comment"))
                     record = token + "\t"+"\t"+"\t" + is;
                     else
